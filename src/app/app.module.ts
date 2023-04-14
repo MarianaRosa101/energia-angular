7a4e64ea0ad348eac3e1013440ee53ee0193b003
MarianaRosa101 <marianarosa100@gmail.com>
2023-04-13 21:49:48 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnergiaComponent } from './energia/energia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnergiaComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
